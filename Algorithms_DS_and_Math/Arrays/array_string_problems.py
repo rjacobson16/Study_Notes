5d6ee7654f1d418d3c2b3020c1f34d02d038dca0
Rachel Jacobson <rmjacobson1@gmail.com>
2017-11-01 17:06:18 -0700

    missing array member in python

--- Algorithms_DS_and_Math/Arrays/array_string_problems.py
+++ Algorithms_DS_and_Math/Arrays/array_string_problems.py
@@ -17,3 +17,20 @@ def anagram(str1, str2):
         str2_count[c] +=1
 
     return str1_count == str2_count
+
+def missing_member(arr1, arr2):
+    arr1_count, arr2_count = Counter(), Counter()
+
+    for num in arr1:
+
+        arr1_count[num] +=1
+
+    for num in arr2:
+
+        arr2_count[num] +=1
+
+    for k,v in arr1_count.items():
+        if not arr2_count[k] == arr1_count[k]:
+            return k
+
+    return None
