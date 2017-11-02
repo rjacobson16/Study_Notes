7f4d9b9ea5ff7c26e653726a9fd966ca828c2bbf
Rachel Jacobson <rmjacobson1@gmail.com>
2017-11-02 11:30:00 -0700

    max contig subarray in python

--- Algorithms_DS_and_Math/Arrays/array_string_problems.py
+++ Algorithms_DS_and_Math/Arrays/array_string_problems.py
@@ -34,3 +34,32 @@ def missing_member(arr1, arr2):
             return k
 
     return None
+
+def large_cont_sum(arr):
+    if len(arr) == 0:
+        return 0
+
+    max_so_far = arr[0]
+    max_ending_here = 0
+    max_element = arr[0]
+    start_idx = end_idx = current_idx = 0
+
+    for i, v in enumerate(arr):
+        if v > max_element:
+            max_element = v
+            max_idx = i
+
+        max_ending_here += v
+        if max_ending_here < 0:
+           max_ending_here = 0
+           current_idx = i+1
+
+        if max_so_far < max_ending_here:
+            max_so_far = max_ending_here
+            start_idx = current_idx
+            end_idx = i
+
+    if max_so_far > 0:
+        return f"The largest continuous subarray is {arr[start_idx:end_idx]}, which sums to {max_so_far}"
+    else:
+        return f"All Neg: The largest continuous subarray is {[arr[max_idx]]}, which sums to {max_element}"
